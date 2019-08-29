99471c84a3a0a0b9ffd5cf473fdcf1343f4159ed
ryo-chin <h031203yama@gmail.com>
2019-08-29 21:56:02 +0900

    initialize project

---
+++ src/main/kotlin/com/hakiba/spacucumber/SpaCucumberApplication.kt
@@ -0,0 +1,11 @@
+package com.hakiba.spacucumber
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpaCucumberApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpaCucumberApplication>(*args)
+}
---
+++ src/test/kotlin/com/hakiba/spacucumber/SpaCucumberApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.hakiba.spacucumber
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpaCucumberApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
