49b3584b78746cbea5b3b69d306dc6c1494f5372
ryo-chin <h031203yama@gmail.com>
2019-09-03 00:18:47 +0900

    Add comment.

--- src/main/kotlin/com/hakiba/spacucumber/Logger.kt
+++ src/main/kotlin/com/hakiba/spacucumber/Logger.kt
@@ -6,4 +6,5 @@ import org.slf4j.LoggerFactory
 /**
  * @author hakiba
  */
+// ref: https://www.baeldung.com/kotlin-logging
 fun <T : Any> T.prepareLogger(): Logger = LoggerFactory.getLogger(javaClass)
