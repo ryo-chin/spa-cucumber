1752b87ad5cc7b64f8ac5e749a168b7093016ba9
ryo-chin <h031203yama@gmail.com>
2019-10-14 15:51:09 +0900

    [fix] remove unnecessary setting from SecurityConfig

--- api-server/src/main/kotlin/com/hakiba/spacucumber/spring/SecurityConfig.kt
+++ api-server/src/main/kotlin/com/hakiba/spacucumber/spring/SecurityConfig.kt
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.core.session.SessionRegistry
-import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2Error
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
 import org.springframework.security.oauth2.jwt.*
-import org.springframework.security.web.savedrequest.NullRequestCache
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -21,7 +19,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
  * @author hakiba
  */
 @Configuration
-class SecurityConfig: WebSecurityConfigurerAdapter() {
+class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Value("\${auth0.audience}")
     private val audience: String? = null
@@ -32,6 +30,7 @@ class SecurityConfig: WebSecurityConfigurerAdapter() {
     @Value("\${spring.security.allowedOriginUrl}")
     private val allowedOriginUrl: String? = null
 
+    // Auth0 + SpringSecurity5 ref: https://auth0.com/docs/quickstart/backend/java-spring-security5/01-authorization
     @Bean
     fun jwtDecoder(): JwtDecoder {
         val jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer!!) as NimbusJwtDecoderJwkSupport
@@ -44,28 +43,13 @@ class SecurityConfig: WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
+        // Auth0 + SpringSecurity5 ref: https://auth0.com/docs/quickstart/backend/java-spring-security5/01-authorization
         http.oauth2ResourceServer().jwt()
-        http.cors().configurationSource(corsConfigurationSource())
-        http
-                .csrf().disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/graphql").authenticated()
-                .and()
-                .requestCache()
-                .requestCache(NullRequestCache())
-                .and()
-                .headers()
-                .frameOptions().sameOrigin() // needed for H2 web console
-                .and()
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true)
-                .sessionRegistry(sessionRegistry())
-    }
-
-    @Bean
-    fun sessionRegistry(): SessionRegistry {
-        return SessionRegistryImpl()
+        // no needs session since use token auth
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.cors().configurationSource(corsConfigurationSource())
     }
 
     private fun corsConfigurationSource(): CorsConfigurationSource {
@@ -80,6 +64,7 @@ class SecurityConfig: WebSecurityConfigurerAdapter() {
     }
 }
 
+// Auth0 + SpringSecurity5 ref: https://auth0.com/docs/quickstart/backend/java-spring-security5/01-authorization
 class AudienceValidator(audience: String?) : OAuth2TokenValidator<Jwt> {
     companion object {
         private var audience: String? = null
